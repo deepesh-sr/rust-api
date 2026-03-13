77523c74c6f1170b8ae3b4ddb01925a4d873a82c
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-13 16:35:40 +0530

    feat: get method added , db connected

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use axum::routing::get;
+use dotenv;
+use std::env;
+use std::error::Error;
+
+use axum::{Json, Router};
+use serde::Serialize;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{FromRow, PgPool};
+
+struct AppState {
+    pool: PgPool,
+}
+
+#[derive(Serialize, FromRow)]
+struct Project {
+    id: i32,
+    title: String,
+    description: String,
+    is_completed: bool,
+    is_created: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv::dotenv().ok();
+    let db_url = env::var("DATABASE_URL")?;
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(db_url.as_str())
+        .await?;
+
+    let app = Router::new().route("/", get(|| async {"Hello Deepesh"}));
+        // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
 }
