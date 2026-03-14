ef32aa4cbd98dfb2dad6b1f1dd7c850622c7e8a3
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-03-14 17:06:02 +0530

    feat: add routes for get project and delete project by :id

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::extract::State;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
 use axum::routing::get;
 use dotenv;
@@ -8,7 +8,7 @@ use chrono::{DateTime, Utc};
 
 use axum::{Json, Router};
 use serde::{Deserialize, Serialize};
-use sqlx::postgres::PgPoolOptions;
+use sqlx::postgres::{PgPoolOptions, PgQueryResult};
 use sqlx::{FromRow, PgPool};
 
 #[derive(Clone)]
@@ -31,6 +31,7 @@ struct CreateProject {
     description : String
 }
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv::dotenv().ok();
@@ -45,8 +46,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let state = AppState{pool};
     let app = Router::new()
     .route("/", get(|| async { "Hello Deepesh" }))
-    .route("/projects", get(get_projects).post(create_project)
-    .with_state(state));
+    .route("/projects", get(get_projects).post(create_project))
+    .route("/project/:id", get(get_project_by_id).delete(delete_project_by_id))
+    .with_state(state);
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:4444").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -59,6 +61,18 @@ async fn get_projects(State(state): State<AppState>)-> Result<Json<Vec<Project>>
   Ok(Json(projects))
 }
 
+async fn get_project_by_id(State(state): State<AppState>, Path(id): Path<i32>)->Result<Json<Project>, StatusCode>{
+    let project = sqlx::query_as::<_,Project>("SELECT * FROM projects WHERE id= $1").bind(id).fetch_optional(&state.pool).await.map_err(|_| StatusCode::NO_CONTENT)?;
+    match project { 
+        Some(p) => Ok(Json(p)),
+        None => Err(StatusCode::NOT_FOUND)
+    }
+}
+
+async fn delete_project_by_id(State(state): State<AppState>, Path(id): Path<i32>)->Result<StatusCode, StatusCode>{
+    sqlx::query("DELETE FROM projects WHERE id = $1").bind(id).execute(&state.pool).await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+   Ok(StatusCode::OK) 
+}
 
 async fn create_project(State(state):State<AppState> , input_query : Json<CreateProject>)->Result<StatusCode, StatusCode>{
     let title = &input_query.title;
